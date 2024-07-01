0efe3dfc4872a6828a042ac5dae70a5acea86e04
Felipe Mora <gnar2342@gmail.com>
2024-07-01 17:23:54 -0400

    [ADD] mejora visual eliminar gastos.

---
+++ envExcel/app/static/js/gastos.js
@@ -0,0 +1,18 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const elementoDiv = document.getElementById('containerGastos');
+    const altoVisible = elementoDiv.clientHeight;
+    const altoTotal = elementoDiv.scrollHeight;
+    const flechaAbajo = document.querySelector('.container-flecha-abajo');
+    const containerGastos = document.querySelectorAll('.container-gasto');
+    
+
+    if(altoTotal <= altoVisible){
+        flechaAbajo.style.display = 'none';
+    }
+
+    containerGastos.forEach((gasto) => {
+        gasto.addEventListener('click', () => {
+            gasto.classList.toggle('gasto-agregado');
+        });
+    })
+})
